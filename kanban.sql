616f8e6a596eb87f2f326bd9ec1cc9914ff496c2
Alessandro <alessandroamantini7@gmail.com>
2024-05-03 18:53:13 +0200

    aggiunto db fantoccio

---
+++ kanban.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 03, 2024 alle 18:52
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kanban`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `stati`
+--
+
+CREATE TABLE `stati` (
+  `nome` varchar(500) DEFAULT NULL,
+  `stati` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `stati`
+--
+
+INSERT INTO `stati` (`nome`, `stati`) VALUES
+('Sistemare la scrivania', 1),
+('Fare la spesa', 2),
+('Studiare per lesame', 3),
+('Allenarsi in palestra', 4),
+('Preparare la cena', 1),
+('Fare il bucato', 2),
+('Mandare figlio a scuola', 3);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
